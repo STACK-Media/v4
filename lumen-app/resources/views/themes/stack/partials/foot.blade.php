c239ecc33b1adf808dc728d1d8fb1765afd93dc4
Matt Thompson <matt.thompson@stack.com>
2015-06-23 14:26:59 -0400

    created some widgets

---
+++ lumen-app/resources/views/themes/stack/partials/foot.blade.php
@@ -0,0 +1,36 @@
+<div class="row">
+
+	<div class="col-xs-12 col-sm-12">
+
+		<?php 
+		echo view('theme::partials.widgets.footer-links');
+		?>
+
+	</div>
+
+</div>
+
+<div class="row">
+
+		<div class="col-xs-12">
+
+			<center><img src="/assets/img/partners/yardbarker.gif" /></center>
+
+		</div>
+
+</div>
+
+<div class="row">
+
+		<div class="col-xs-12">
+
+			<div class="spacer"></div>
+
+		</div>
+
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="/assets/js/viewport.js"></script>
+<script src="/assets/js/events.js"></script>
