f597b4f3609bbf860f1d139e5db20ae5c51d519a
Matt Thompson <matt.thompson@stack.com>
2015-06-23 15:31:53 -0400

    created some widgets

---
+++ lumen-app/resources/views/themes/stack/subthemes/4w/partials/navbar.blade.php
@@ -0,0 +1,10 @@
+<!-- Navbar -->
+<nav class="navbar navbar-inverse bottom-red legroom">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">
+				<img alt="STACK" src="/assets/img/branding/logos/large_white.png" width="160" height="35"> 4W
+			</a>
+		</div>
+	</div>
+</nav>	
