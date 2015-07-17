76189d826b5e2b3a124208d6edeaa0f6b42cb9b1
Matt Thompson <matt.thompson@stack.com>
2015-07-17 10:45:29 -0400

    removed debug

--- lumen-app/app/Services/WidgetServices/OutbrainSidebar.php
+++ lumen-app/app/Services/WidgetServices/OutbrainSidebar.php
@@ -6,10 +6,6 @@ class Outbrain extends WidgetService
 {
 	public function get($page)
 	{
-print "<pre>";
-print_r($page);
-exit;
-
 		// determine which outbrain URL to use
 		$outbrain_url	= 'www.stack.com';
 
