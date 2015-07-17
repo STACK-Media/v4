36f205027e4e4a26ac017b6f43f6309e9109743f
Travman <travis.loudin@stack.com>
2015-07-17 11:08:00 -0400

    Added service

--- lumen-app/app/Services/Videoplayer.php
+++ lumen-app/app/Services/Videoplayer.php
@@ -5,7 +5,7 @@ namespace App\Services;
 class Videoplayer extends Service
 {
 
-	private $_class = 'Brightcove';
+	private $_class = 'Eatdickmatt';
 
 	protected $_player;
 
---
+++ lumen-app/app/Services/Videoplayers/Eatdickmatt.php
@@ -0,0 +1,8 @@
+<?php 
+
+namespace App\Services\Videoplayers;
+
+class Eatdickmatt extends Player
+{
+
+}
