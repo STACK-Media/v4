3d6cbcbce890a2f7c21d70a072f3612756756179
Travman <travis.loudin@stack.com>
2015-07-17 14:41:12 -0400

    Cleaned up asset path stuff

--- lumen-app/resources/views/themes/v4/partials/widgets/player.blade.php
+++ lumen-app/resources/views/themes/v4/partials/widgets/player.blade.php
@@ -2,8 +2,9 @@
 
 {!! Assets::queue('javascript', 'player', '/assets/js/widgets/player.js') !!}
 
-
+<?php /*
 {!! Assets::queue('stylesheet', 'testing', Assets::themed('css/testing_themepath.css')) !!}
+*/ ?>
 
 @if(view()->exists('theme::partials.videoplayers.'.$player_name))
 
