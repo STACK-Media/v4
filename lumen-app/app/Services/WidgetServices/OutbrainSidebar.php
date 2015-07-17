f78951a9d5ac3c12a951874fdc4aac050e22dcbe
Matt Thompson <matt.thompson@stack.com>
2015-07-17 10:44:59 -0400

    updated outbrain sidebar widget service

--- lumen-app/app/Services/WidgetServices/OutbrainSidebar.php
+++ lumen-app/app/Services/WidgetServices/OutbrainSidebar.php
@@ -2,9 +2,7 @@
 
 namespace App\Services\WidgetServices;
 
-use App\Services\Service;
-
-class OutbrainSidebar extends Service
+class Outbrain extends WidgetService
 {
 	public function get($page)
 	{
