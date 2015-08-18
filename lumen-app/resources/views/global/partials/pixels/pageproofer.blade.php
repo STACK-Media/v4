1514be34554ce9bddfda297909a91956631391e7
Travman <travis.loudin@stack.com>
2015-08-18 15:09:25 -0400

    Fixing pageproofer ip

--- lumen-app/resources/views/global/partials/pixels/pageproofer.blade.php
+++ lumen-app/resources/views/global/partials/pixels/pageproofer.blade.php
@@ -1,4 +1,4 @@
-@if(in_array(Request::getClientIp(), array('127.0.0.1', '192.168.1.112', '74.141.98.100')))
+@if(in_array(Request::getClientIp(), array('127.0.0.1', '192.168.1.112', '74.141.98.100', '104.179.86.19')))
 <script type="text/javascript">
 (function (d, t) {
    var pp = d.createElement(t), s = d.getElementsByTagName(t)[0];
