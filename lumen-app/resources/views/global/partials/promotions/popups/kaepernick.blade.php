87b510499c3bcb71f9fef3e054ee20bc1480f575
Travman <travis.loudin@stack.com>
2015-09-15 14:52:09 -0400

    Added close button to kaepernick popup

--- lumen-app/resources/views/global/partials/promotions/popups/kaepernick.blade.php
+++ lumen-app/resources/views/global/partials/promotions/popups/kaepernick.blade.php
@@ -9,27 +9,29 @@
 
 <form id="kaepernick" method="post" action="">
 
+	<div id="kaepclose" class="close" data-dismiss="modal">&times;</div>
+
 	<div class="row">
 
-	<div id="kaepinner" class="col-md-7 pull-right">
+		<div id="kaepinner" class="col-md-7 pull-right">
 
-		<input type="hidden" value="1" name="lists[Newsletter]">
-		<input type="hidden" value="1" name="lists[RobotNewsletter]">
-		<input type="hidden" value="newsletter_popup_mobile" name="vars[source]">
-		<input type="hidden" value="" name="vars[keywords]">
+			<input type="hidden" value="1" name="lists[Newsletter]">
+			<input type="hidden" value="1" name="lists[RobotNewsletter]">
+			<input type="hidden" value="newsletter_popup_mobile" name="vars[source]">
+			<input type="hidden" value="" name="vars[keywords]">
 
-		<p>Become a Better Athlete.</p>
-		<p>Sign Up for our FREE Newsletter.</p>
+			<p>Become a Better Athlete.</p>
+			<p>Sign Up for our FREE Newsletter.</p>
 
-		<input type="text" placeholder="Your Email" name="email" class="form-control" id="kaepemail">
+			<input type="text" placeholder="Your Email" name="email" class="form-control" id="kaepemail">
 
-		<button type="submit" class="btn btn-danger btn-block">
-			Sign Up
-		</button>
+			<button type="submit" class="btn btn-danger btn-block">
+				Sign Up
+			</button>
+
+		</div>
 
 	</div>
-
-</div>
 
 </form>
 
