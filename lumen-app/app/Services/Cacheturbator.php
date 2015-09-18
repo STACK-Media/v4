a4882c6e12845d89a54ba7ef69b8b2ea7e45ca75
Travman <travis.loudin@stack.com>
2015-09-18 13:23:51 -0400

    Testing cache key changes

--- lumen-app/app/Services/Cacheturbator.php
+++ lumen-app/app/Services/Cacheturbator.php
@@ -44,7 +44,7 @@ class Cacheturbator extends Service
 
 			if ( ! $this->flush && Cache::has($cache_key)):
 
-				return Cache::get($cache_key);
+				return unserialize(Cache::get($cache_key));
 
 			endif;
 
@@ -54,7 +54,7 @@ class Cacheturbator extends Service
 
 		if ($this->cache_on):
 
-			Cache::put($cache_key, $result, mt_rand($this->min_cache, $this->max_cache));
+			Cache::put($cache_key, serialize($result), mt_rand($this->min_cache, $this->max_cache));
 
 		endif;
 
@@ -82,7 +82,7 @@ class Cacheturbator extends Service
 			$cache_key = 's.'.$this->_get_class_name().'.a.'.$attrib_name.'.v.'.json_encode($value);
 			$cache_key = $this->_limit_key_size($cache_key);
 
-			Cache::put($cache_key, $value, mt_rand($this->min_cache, $this->max_cache));
+			Cache::put($cache_key, serialize($value), mt_rand($this->min_cache, $this->max_cache));
 
 		endif;
 
@@ -126,7 +126,7 @@ class Cacheturbator extends Service
 
 			if ( ! $this->flush && Cache::has($cache_key)):
 
-				return Cache::get($cache_key);
+				return unserialize(Cache::get($cache_key));
 
 			endif;
 
@@ -136,7 +136,7 @@ class Cacheturbator extends Service
 
 		if ($this->cache_on):
 
-			Cache::put($cache_key, $result, mt_rand($this->min_cache, $this->max_cache));
+			Cache::put($cache_key, serialize($result), mt_rand($this->min_cache, $this->max_cache));
 
 		endif;
 
