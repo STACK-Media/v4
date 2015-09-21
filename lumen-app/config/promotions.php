46a002641f00fef4c22bb4b3aef75e2abaf27480
Matt Thompson <matt.thompson@stack.com>
2015-09-21 11:34:06 -0400

    updated paid marketing pages to not fire popup

--- lumen-app/config/promotions.php
+++ lumen-app/config/promotions.php
@@ -26,6 +26,9 @@ return array(
 				),*/
 				// promotion can only have "blacklist" or "whitelist" - if not whitelist, it'll be global everywhere except the blacklist
 				'kaepernick' => array(
+					'blacklist'	=> array(
+						'marketing'
+					),
 					'views'     => array( // randomly select one view from array
 						'global.partials.promotions.popups.kaepernick',
 					),
