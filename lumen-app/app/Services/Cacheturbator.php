c26914ce1ab88cf160b6d5e2649cf52b95a8fd43
Travman <travis.loudin@stack.com>
2015-09-22 11:31:26 -0400

    Testing cache put further

--- lumen-app/app/Services/Cacheturbator.php
+++ lumen-app/app/Services/Cacheturbator.php
@@ -109,9 +109,13 @@ class Cacheturbator extends Service
 
 		if ($this->cache_on):
 
-			file_put_contents('/var/www/CACHE_PUT_LOG.log', '--------CACHE_KEY--------'."\n".$cache_key."\n".'--------CACHE_BODY--------'."\n".serialize($result)."\n".'--------CACHE_EXPIRY--------'."\n".mt_rand($this->min_cache, $this->max_cache)."\n=========================\n\n\n\n", FILE_APPEND);
+			//file_put_contents('/var/www/CACHE_PUT_LOG.log', '--------CACHE_KEY--------'."\n".$cache_key."\n".'--------CACHE_BODY--------'."\n".serialize($result)."\n".'--------CACHE_EXPIRY--------'."\n".mt_rand($this->min_cache, $this->max_cache)."\n=========================\n\n\n\n", FILE_APPEND);
 
-			//Cache::put($cache_key, serialize($result), mt_rand($this->min_cache, $this->max_cache));
+			if ($result):
+
+			Cache::put($cache_key, serialize($result), mt_rand($this->min_cache, $this->max_cache));
+
+			endif;
 
 		endif;
 
