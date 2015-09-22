a1c0007fc313b61b13b313fd683668f1e9cef6d2
Matt Thompson <matt.thompson@stack.com>
2015-09-22 10:38:33 -0400

    added tos

---
+++ lumen-app/resources/views/themes/v4/custom/terms-of-use.blade.php
@@ -0,0 +1,14 @@
+@extends('theme::layouts.two_column')
+
+{!! Assets::queue('stylesheet', 'layout', 'terms', '/assets/css/custom/terms.css') !!}
+
+@section('content')
+
+	<section class="atoz">
+
+		<h1>Terms Of Use</h1>
+
+	</section>
+
+
+@stop
