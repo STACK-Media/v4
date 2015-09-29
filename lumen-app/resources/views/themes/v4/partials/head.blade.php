dbe8c7a5ea1913fff26d4549ad95312fc3264f75
Matt Thompson <matt.thompson@stack.com>
2015-09-29 09:07:11 -0400

    testing meta tags

--- lumen-app/resources/views/themes/v4/partials/head.blade.php
+++ lumen-app/resources/views/themes/v4/partials/head.blade.php
@@ -23,6 +23,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta property="og:site_name" content="STACK" name="og:site_name" />
 
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@STACKMedia">
+<meta name="twitter:title" content="Parade of Fans for Houston’s Funeral">
+<meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.">
+<meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg">
+
 
 <title>{!! (is_object($page) && $page->name) ? $page->name . ' |' : ''; !!} STACK</title>
 
