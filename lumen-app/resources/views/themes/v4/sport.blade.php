5cd90685b3c8a2feb5eaccf761bca656708d6cdf
Matt Thompson <matt.thompson@stack.com>
2015-09-30 13:29:33 -0400

    updated style for sports-workouts widget

--- lumen-app/resources/views/themes/v4/sport.blade.php
+++ lumen-app/resources/views/themes/v4/sport.blade.php
@@ -8,6 +8,8 @@
 		<h1>{!! $page->name !!}</h1>
 		<p><i>{!! $page->description !!}</i></p>
 
+		<hr>
+
 		@if(@$page->player)
 
 			@include('theme::partials.videoplayers.'.$page->player['player_name'], $page->player['player_data'])
