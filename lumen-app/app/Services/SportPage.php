517e64b6e79c9c580d86eba383c7d4f49e308b49
Matt Thompson <matt.thompson@stack.com>
2015-09-30 12:02:28 -0400

    added custom meta to sport specific page template

--- lumen-app/app/Services/SportPage.php
+++ lumen-app/app/Services/SportPage.php
@@ -27,8 +27,28 @@ class SportPage extends Page
 		$this->_object->player    	= $player->get($this->_object->meta['stackvideoid']);
 		$this->_object->video 		= $this->_object->meta['stackvideoid'];
 
+		$this->_object->metatags 	= $this->_get_metatags();
+
 		return parent::__construct();
 	}
 
+	private function _get_metatags()
+	{
+		// initialize variables
+		$metatags 	= array();
+
+		$metatags['title']			= ucwords($this->_object->sport).' Workouts | '.ucwords($this->_object->sport).' Drills';
+		$metatags['description']	= 'Find '.$this->_object->sport.' workouts, drills, training advice and nutrition tips updated daily on STACK.com.';
+		$metatags['keywords']		= array(
+			'workouts',
+			'drills',
+			'training',
+			'nutrition',
+			'sports',
+			$this->_object->sport
+		);
+
+		return $metatags;
+	}
 	
 } 
