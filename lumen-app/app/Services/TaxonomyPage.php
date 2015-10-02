79e074ceaf95db81a2163c608baaa9980b633d8c
Travman <travis.loudin@stack.com>
2015-10-02 17:24:47 -0400

    Testing

--- lumen-app/app/Services/TaxonomyPage.php
+++ lumen-app/app/Services/TaxonomyPage.php
@@ -21,7 +21,7 @@ class TaxonomyPage extends Page
 
 		$this->_object = $cms->get_by_column($type, $identifier_type, $identifier);
 
-		var_dump($this->_object); exit();
+		//var_dump($this->_object); exit();
 
 		if (is_object($this->_object) && property_exists($this->_object, 'id')):
 
